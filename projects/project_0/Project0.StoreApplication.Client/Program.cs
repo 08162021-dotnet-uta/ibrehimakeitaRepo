30ec93a0a1c3c459ab6458c700072a1537a0a038
ibrehima keita <ibrehima.keita@revature.net>
2021-08-20 14:06:58 -0400

    Xnit has been added and StoreRepository had passed the test

--- projects/project_0/Project0.StoreApplication.Client/Program.cs
+++ projects/project_0/Project0.StoreApplication.Client/Program.cs
@@ -41,3 +41,4 @@ namespace Project0.StoreApplication.Client
     }
   }
 }
+
---
+++ projects/project_0/Project0.StoreApplication.Testing/StoreRepository.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using Project0.StoreApplication.Storage.Repositories;
+
+namespace Project0.StoreApplication.Testing
+{
+  public class StoreRepositoryTests
+  {
+    [Fact]
+    public void Tes_StoreCollection()
+    {
+      // arrange = instance of the entity to test
+      var sut = new StoreRepository();
+      // act = excecute sut for data
+      var actual = sut.Stores;
+      //assert
+      Assert.NotNull(actual);
+    }
+  }
+
+}
